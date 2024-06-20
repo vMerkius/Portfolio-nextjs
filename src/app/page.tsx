2b8e553fe1637d50a6ad985ae59422fbb2a6c5bd
v Merkius <Merkius@wp.pl>
2024-06-20 22:19:23 +0200

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+// import ThreeScene from "@/components/three-scene";
+import "../styles/welcome.scss";
+const WelcomePage = () => {
+  return (
+    <div className="welcome">
+      <h1 className="welcome__header">Welcome to my portfolio!</h1>
+      {/* <ThreeScene /> */}
+    </div>
+  );
+};
+
+export default WelcomePage;
