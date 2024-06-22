742a561d9f26783706629e500afda937aca51003
Mateusz Rojek <Merkius@wp.pl>
2024-06-22 20:02:27 +0200

    welcome page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ const WelcomePage = () => {
   return (
     <div className="welcome">
       <h1 className="welcome__header">Welcome to my portfolio!</h1>
+      <h2 className="welcome__next">Let's see! &rarr;</h2>
       {/* <ThreeScene /> */}
     </div>
   );
