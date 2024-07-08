a21f2e2a508d951f254b1eed74b8e19d0c4c03ec
Mateusz Rojek <Merkius@wp.pl>
2024-07-08 16:49:42 +0200

    fix

--- src/components/education.tsx
+++ src/components/education.tsx
@@ -5,12 +5,12 @@ const Education = () => {
     <div className="education">
       <h3 className="education__header">Education</h3>
       <div className="education__item">
-        <strong>&#8226; Bachelor's Degree in Computer Science</strong>
+        <strong>&#8226; Bachelor&apos;s Degree in Computer Science</strong>
         <p>10.2020 - 02.2024</p>
         <p>Bialystok University of Technology</p>
       </div>
       <div className="education__item">
-        <strong>&#8226; Master's Degree in Computer Science</strong>
+        <strong>&#8226; Master&apos;s Degree in Computer Science</strong>
         <p>03.2024 - present</p>
         <p>Bialystok University of Technology</p>
       </div>
